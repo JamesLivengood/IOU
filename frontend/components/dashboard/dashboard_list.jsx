2e0bed164832fc9045296a14724aab7564bfc13f
James Livengood <james.livengood@gmail.com>
2018-03-16 12:22:56 -0400

    homepage css

--- frontend/components/dashboard/dashboard_list.jsx
+++ frontend/components/dashboard/dashboard_list.jsx
@@ -42,7 +42,7 @@ const DashboardListItem = ({bill, info, owed, openModal, fetchBill}) => {
         <img className='dashboard-list-icon' src='https://dx0qysuen8cbs.cloudfront.net/assets/fat_rabbit/avatars/50-31b0bb2f5aec77f11d60a1dc3fa14c23a958fed79261b32e94a73e9c27473ebb.png'></img>
         <div className='dashboard-list-info'>
           <div className='dashboard-list-name'>{info.name}</div>
-          <div className='dashboard-list-amount'>{oweOrOwed} <span className='bold-balance'>${info.balance.toFixed(2)}</span></div>
+          <div className='dashboard-list-amount'>{oweOrOwed} <span className='bold-balance'>${Math.abs(info.balance).toFixed(2)}</span></div>
         </div>
       </div>
     </button>
