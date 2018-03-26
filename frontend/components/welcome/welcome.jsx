175aa231493e1bd6c8fe5fae62b6bed32d1af936
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-03-26 15:33:07 -0400

    fix user dropdown modal positioning with zoom

--- frontend/components/welcome/welcome.jsx
+++ frontend/components/welcome/welcome.jsx
@@ -6,17 +6,15 @@ import {ensureModalOff} from './home_page';
 import { withRouter } from 'react-router-dom';
 
 const Welcome = ({ currentUser, modal, logout, openModal, closeModal, history }) => {
-//  
   return (
-    currentUser ? userLoggedIn(currentUser, openModal, logout) : sessionLinks(modal, openModal, closeModal, history)
+    currentUser ? userLoggedIn(modal, currentUser, openModal, closeModal, logout) : sessionLinks(modal, openModal, closeModal, history)
   );
 };
 
 const sessionLinks = (modal, openModal, closeModal, history) => {
-//  
   return (
     <ul className="login-signup">
-      <li className="login-button-li"><button className='login-button' onClick={()=>modalSwitch(modal, openModal, closeModal)}>Log in</button>
+      <li className="login-button-li"><button className='login-button' onClick={()=>modalSwitch(modal, openModal, closeModal, 'login')}>Log in</button>
         <Modal/>
       </li>
       <li className="or">or</li>
@@ -25,30 +23,28 @@ const sessionLinks = (modal, openModal, closeModal, history) => {
   );
 };
 
-const userLoggedIn = (props, openModal, logout) => {
-  //  
+const userLoggedIn = (modal, currentUser, openModal, closeModal, logout) => {
   let toggleObj = {};
   toggleObj['toggle'] = "hidden-user-dropdown";
-//  
 	return (<hgroup className="header-group">
     <div className="user-button-div">
-      <button className="user-dropdown-button" onClick={()=>openModal('userDropdown')}>
+      <button className="user-dropdown-button" onClick={()=>modalSwitch(modal, openModal, closeModal, 'userDropdown')}>
         <img src='https://dx0qysuen8cbs.cloudfront.net/assets/fat_rabbit/avatars/50-31b0bb2f5aec77f11d60a1dc3fa14c23a958fed79261b32e94a73e9c27473ebb.png'/>
-        <span>{props.name}</span>
+        <span>{currentUser.name}</span>
         <div className='caret'>▼</div>
+        <Modal logout={logout}/>
       </button>
-      <Modal logout={logout}/>
     </div>
 	</hgroup>);
 };
 
-function modalSwitch(modal, openModal, closeModal) {
-//  
+function modalSwitch(modal, openModal, closeModal, whichModal) {
+//
   if (modal) {
     return closeModal();
   } else
     {
-    return openModal('login');
+    return openModal(whichModal);
   }
 };
 
